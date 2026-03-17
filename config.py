036201cb2604579587da0eaf31dacc11ed10c685
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2026-03-17 11:17:35 +0800

    Standardize data storage to fix boot crash

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ except ImportError:
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'stockpro_secret_key_v1')
-    DB_PATH = os.environ.get('DB_PATH', os.path.join(os.path.dirname(__file__), 'database.db'))
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(os.path.dirname(__file__), 'uploads'))
+    DB_PATH = os.environ.get('DB_PATH', os.path.join(os.path.dirname(__file__), 'data', 'database.db'))
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(os.path.dirname(__file__), 'data', 'uploads'))
     TEMPLATE_PATH = os.environ.get('TEMPLATE_PATH', os.path.join(os.path.dirname(__file__), 'template.xlsx'))
     CORS_ORIGINS = os.environ.get('CORS_ORIGINS', 'http://localhost:3000').split(',')
     DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
